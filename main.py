10688793c913b16b1f0ce323ab09b3d0118b649c
LabGames <99832300+LabGames@users.noreply.github.com>
2025-07-30 16:55:37 -0500

    update commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Ventana en blanco")
+    root.geometry("600x400")  # Tamaño inicial
+    root.mainloop()
+
