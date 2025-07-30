f09912ac48b87374d956d432faf200a1938754e8
LabGames <99832300+LabGames@users.noreply.github.com>
2025-07-30 17:20:12 -0500

    update commit

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from tkinter import ttk
 if __name__ == "__main__":
     root = Tk()
     root.title("App de Base de Datos")
-    root.geometry("600x400")
+    # Obtener resolución de pantalla
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    root.geometry(f"{screen_width}x{screen_height}+0+0")
+    root.attributes('-fullscreen', True)
     root.mainloop()
-
