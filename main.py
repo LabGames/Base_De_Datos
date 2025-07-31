97efc16dd71a6d7947fad67e8ce494899cfeba0f
LabGames <99832300+LabGames@users.noreply.github.com>
2025-07-31 09:25:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
 if __name__ == "__main__":
     root = Tk()
-    root.title("App de Base de Datos")
+    root.title("Sistema de Venta de Aplicaciones")
+
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     root.geometry(f"{screen_width}x{screen_height}+0+0")
@@ -11,6 +13,14 @@ if __name__ == "__main__":
 
     def exit_app(event=None):
         root.destroy()
+
+    etiqueta_cliente = tk.Label(root, text="Nombre del Cliente:")
+    etiqueta_cliente.pack()
+    entrada_cliente = tk.Entry(root)
+    entrada_cliente.pack()
+
+    boton_confirmar = tk.Button(root, text="Confirmar Venta", command=lambda: print("Venta realizada"))
+    boton_confirmar.pack()
 
     root.bind('<Escape>', exit_app)
     root.mainloop()
