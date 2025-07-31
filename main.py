bb524e0c2ff773d94a365334a907fe319477bb76
LabGames <99832300+LabGames@users.noreply.github.com>
2025-07-31 09:18:45 -0500

    update commit

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ if __name__ == "__main__":
     screen_height = root.winfo_screenheight()
     root.geometry(f"{screen_width}x{screen_height}+0+0")
     root.attributes('-fullscreen', True)
+
+    def exit_app(event=None):
+        root.destroy()
+
+    root.bind('<Escape>', exit_app)
     root.mainloop()
